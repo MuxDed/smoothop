13d539ceeaf84bd6ea9e428610dd4b49fe96d4aa
MuxDed <dyuldinm@tcd.ie>
2021-03-08 13:29:25 +0000

    Added login screen WIP

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,33 @@
 <template>
-  <h1>Hello World again</h1>
+  <div class="vue-tempalte">
+    <!-- Navigation -->
+    <nav class="navbar shadow bg-white rounded justify-content-between flex-nowrap flex-row fixed-top">
+      <div class="container">
+        <a class="navbar-brand float-left"  target="_blank">
+           SmoothOp
+        </a>
+        <ul class="nav navbar-nav flex-row float-right">
+          <li class="nav-item">
+            <router-link class="nav-link pr-3" to="/">Sign in</router-link>
+          </li>
+          <li class="nav-item">
+            <router-link class="btn btn-outline-primary" to="/signup">Sign up</router-link>
+          </li>
+        </ul>
+      </div>
+    </nav>
+
+    <!-- Main -->
+    <div class="App">
+      <div class="vertical-center">
+
+        <div class="inner-block">
+          <center>
+            <img src="./assets/logo.jpg">
+          </center>
+          <router-view />
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
-
-<script>
-
-
-export default {
-  name: 'App',
-  components: {
-  
-  }
-}
-</script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
